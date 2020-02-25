0535a2693cce261fb16b7aa835cae3c50255f645
rammah karpous <rammah@iweb.co.uk>
2020-02-25 11:23:32 +0000

    Created the modules section

--- js/script.js
+++ js/script.js
@@ -20,4 +20,41 @@ expandCollapseToggleButtons.forEach((button) =>  {
     });
 });
 
-// Form validation
+// Disabling forms
+let forms = document.querySelectorAll('form');
+
+forms.forEach(form => {
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+    });
+});
+
+// Modules toggle functionality
+let modButtons = document.querySelectorAll('.module-input');
+let modList = document.querySelectorAll('.module-info--list');
+let notList;
+let i;
+
+for (i = 0; i < modButtons.length; i++) {
+    let index = i+1;
+    notList = document.querySelectorAll(`.module-info--item:nth-child(${i})`);
+}
+
+modButtons.forEach((modButton) => {
+
+    modButton.addEventListener('click', (e) => {
+
+        let modClass = document.querySelector(`.${modButton.value}`);
+
+        modClass.classList.add('show-modules');
+
+        console.log(modClass);
+
+        // if(modButton.checked) {
+        //     modList.classList.add('this-class');
+        //     notList.classList.remove('this-class');
+        // }
+    });
+});
+
+// Authentication functionality
